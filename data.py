cdba841e30228bcc8f2ab2a6d51038ddea2ee5d1
Steven Wallace <snwallace@gmail.com>
2012-11-19 15:12:57 -0600

    Some parsing.

--- data.py
+++ data.py
@@ -1,13 +1,59 @@
 import yaml
+import structures
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
 
+class GameData(object):
+    def __init__(self, data):
+        self.models = []
+        self.globals = []
+
+        if 'globals' in data:
+            self.parse_globals(data['globals'])
+        if 'models' in data:
+            self.parse_models(data['models'])
+
+    def parse_globals(self, globals):
+        self.globals = [self.parse_var(i) for i in globals.items()]
+
+    def parse_models(self, models):
+        parents = {name: value.get('parent', None) for name, value in models.items()}
+        while parents:
+            eligible = [i for i in parents if parents[i] not in parents]
+            if not eligible:
+                raise ValueError('Circular inheritance tree')
+            for i in eligible:
+                self.models.append(self.parse_model(i, models[i]))
+                del parents[i]
+
+    def parse_var(self, var):
+        name, data = var
+        type = data['type']
+        doc = data.get('doc', '')
+
+        return structures.Variable(name, type, doc)
+
+    def parse_model(self, name, model):
+        data = model.get('data', {})
+        doc = model.get('doc', '')
+        type = model.get('type', 'Model')
+        functions = model.get('functions', {})
+        parent = model.get('parent', None)
+
+        data = [self.parse_var(i) for i in data.items()]
+
+        return structures.Model(name, data=data, doc=doc, type=type,
+                parent=parent)
+
+
 def load(location = 'data.yaml'):
     f = open(location, 'r')
     data = yaml.load(f, Loader)
     print data
+
+    GameData(data)
 
 if __name__ == '__main__':
     load()
