ed388630df3a9c0e5833a4e23f9c46ba7f317709
Steven Wallace <snwallace@gmail.com>
2012-11-22 09:18:05 -0600

    Fixed structures spacing.

--- structures.py
+++ structures.py
@@ -5,74 +5,74 @@ from copy import copy
 models = {}
 
 class Model(object):
-  data = []
-  functions = []
-  properties = []
-  name = ''
-  plural = ''
-  doc = ''
-  type = ''
-  parent = None
-  def __init__(self, name, **kwargs):
-    self.data = [ Variable('id', int, 'Unique Identifier') ]
-    self.functions = []
-    self.properties = []
-    self.name = name
-    self.plural = name + 's'
-    self.type = 'Model'
+    data = []
+    functions = []
+    properties = []
+    name = ''
+    plural = ''
+    doc = ''
+    type = ''
+    parent = None
+    def __init__(self, name, **kwargs):
+        self.data = [ Variable('id', int, 'Unique Identifier') ]
+        self.functions = []
+        self.properties = []
+        self.name = name
+        self.plural = name + 's'
+        self.type = 'Model'
 
-    if 'parent' in kwargs and kwargs['parent']:
-      self.parent = models[kwargs['parent']]
-      self.data = copy(self.parent.data)
-      self.functions = copy(self.parent.functions)
-      self.properties = copy(self.parent.properties)
-    if 'data' in kwargs:
-      data = kwargs['data']
-      self.data.extend(data)
-    if 'functions' in kwargs:
-      functions = kwargs['functions']
-      self.functions.extend(functions)
-    if 'properties' in kwargs:
-      properties = kwargs['properties']
-      self.properties.extend(properties)
-    if 'doc' in kwargs:
-      self.doc = kwargs['doc']
-    if 'type' in kwargs:
-      self.type = kwargs['type']
-    if 'plural' in kwargs:
-      self.plural = kwargs['plural']
+        if 'parent' in kwargs and kwargs['parent']:
+            self.parent = models[kwargs['parent']]
+            self.data = copy(self.parent.data)
+            self.functions = copy(self.parent.functions)
+            self.properties = copy(self.parent.properties)
+        if 'data' in kwargs:
+            data = kwargs['data']
+            self.data.extend(data)
+        if 'functions' in kwargs:
+            functions = kwargs['functions']
+            self.functions.extend(functions)
+        if 'properties' in kwargs:
+            properties = kwargs['properties']
+            self.properties.extend(properties)
+        if 'doc' in kwargs:
+            self.doc = kwargs['doc']
+        if 'type' in kwargs:
+            self.type = kwargs['type']
+        if 'plural' in kwargs:
+            self.plural = kwargs['plural']
 
-    models[name] = self
+        models[name] = self
 
 class Variable(object):
-  name = ''
-  type = None
-  doc = ''
+    name = ''
+    type = None
+    doc = ''
 
-  def __init__(self, name, type, doc=''):
-    self.name = name
-    self.type = type
-    self.doc = doc
+    def __init__(self, name, type, doc=''):
+        self.name = name
+        self.type = type
+        self.doc = doc
 
 class Animation(object):
-  name = ''
-  data = []
+    name = ''
+    data = []
 
-  def __init__(self, name, **kwargs):
-    self.data = []
-    self.name = name
-    if 'data' in kwargs:
-      data = kwargs['data']
-      self.data.extend(data)
+    def __init__(self, name, **kwargs):
+        self.data = []
+        self.name = name
+        if 'data' in kwargs:
+            data = kwargs['data']
+            self.data.extend(data)
 
 
 class Function(object):
-  name = ''
-  arguments = []
-  result = None
-  doc = ''
+    name = ''
+    arguments = []
+    result = None
+    doc = ''
 
-  def __init__(self, name, arguments=[], doc=''):
-    self.name = name
-    self.arguments = arguments
-    self.doc = doc
+    def __init__(self, name, arguments=[], doc=''):
+        self.name = name
+        self.arguments = arguments
+        self.doc = doc
