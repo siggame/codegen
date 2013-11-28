df128a077e5b10b358b317f413be6e6176203864
Steven Wallace <snwallace@gmail.com>
2013-11-27 20:22:35 -0600

    Alphabetic sorting of data.

--- structures.py
+++ structures.py
@@ -1,12 +1,13 @@
 # -*- coding: iso-8859-1 -*-
 from copy import copy
+from operator import attrgetter
+namegetter = attrgetter('name') #Used for sorting
 
 models = {}
 
 class Model(object):
     data = []
     functions = []
-    properties = []
     name = ''
     plural = ''
     doc = ''
@@ -15,7 +16,6 @@ class Model(object):
     def __init__(self, name, **kwargs):
         self.data = [ Variable('id', int, None, None, 'Unique Identifier') ]
         self.functions = []
-        self.properties = []
         self.name = name
         self.plural = name + 's'
         self.type = 'Model'
@@ -24,22 +24,22 @@ class Model(object):
             self.parent = models[kwargs['parent']]
             self.data = copy(self.parent.data)
             self.functions = copy(self.parent.functions)
-            self.properties = copy(self.parent.properties)
         if 'data' in kwargs:
             data = kwargs['data']
             self.data.extend(data)
         if 'functions' in kwargs:
             functions = kwargs['functions']
             self.functions.extend(functions)
-        if 'properties' in kwargs:
-            properties = kwargs['properties']
-            self.properties.extend(properties)
         if 'doc' in kwargs:
             self.doc = kwargs['doc']
         if 'type' in kwargs:
             self.type = kwargs['type']
         if 'plural' in kwargs:
             self.plural = kwargs['plural']
+
+        self.data.sort(key=namegetter)
+        self.functions.sort(key=namegetter)
+        
 
         models[name] = self
 
